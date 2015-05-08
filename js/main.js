3985c7e0b9fff9d8081ad552921965bc446c3e5d
rauduon <superchinois26@gmail.com>
2015-05-08 16:40:29 +0200

    structure projet

--- js/main.js
+++ js/main.js
@@ -9,18 +9,11 @@
         }
     });
 
-    require(['entity-manager',
-                'components/collision',
-                'components/displayable',
-                'components/wonGames',
-                'components/input',
-                'components/life',
-                'components/movable',
-                'components/attack',
-                'components/animation',
-                'components/position',
-                'processors/rendering'],
-            function (EntityManager, Collision, Displayable, WonGames, Input, Life, Movable, Attack, Animation, Position, RenderingProcessor) {
+    require(['states/boot',
+                'states/preloader',
+                'states/game'
+            ],
+            function (Boot, Preloader, Game) {
 
         var game = new Phaser.Game(960, 768, Phaser.AUTO, '', {
             preload: preload,
@@ -29,37 +22,21 @@
             update: update
         });
 
-        // Create an Entity System manager object.
-        var manager = new EntityManager();
-
         function preload() {
             this.game.load.spritesheet('chara_fat', 'assets/gfx/chara_fat.png', 64, 96);
         }
 
         function create() {
-            // Add all components to the system.
-            var components = [
-                Collision,
-                Displayable,
-                WonGames,
-                Input,
-                Life,
-                Movable,
-                Attack,
-                Animation,
-                Position
-            ];
-            for (var i = components.length - 1; i >= 0; i--) {
-                manager.addComponent(components[i].name, components[i]);
-            }
-
-            var player = manager.createEntity(['Position', 'Displayable', 'Movable']);
-
-            manager.addProcessor(new RenderingProcessor(manager, this.game));
         }
 
         function init() {
         }
+
+        game.state.add('Boot', Boot);
+        game.state.add('Preload', Preloader);
+        game.state.add('Game', Game);
+
+        game.state.start('Game');
 
         function update () {
             manager.update(1);
