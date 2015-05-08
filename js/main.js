a23a82e6062e3612bee87c2481e5e87da4783ba4
Adrian Gaudebert <adrian@gaudebert.fr>
2015-05-08 16:48:46 +0200

    Added Phaser-tiled, fixed previous commit.

--- js/main.js
+++ js/main.js
@@ -9,11 +9,12 @@
         }
     });
 
-    require(['states/boot',
-                'states/preloader',
-                'states/game'
-            ],
-            function (Boot, Preloader, Game) {
+    require([
+    'states/boot',
+    'states/preloader',
+    'states/game'
+    ],
+    function (Boot, Preloader, Game) {
 
         var game = new Phaser.Game(960, 768, Phaser.AUTO, '', {
             preload: preload,
@@ -22,24 +23,28 @@
             update: update
         });
 
+        console.log(game);
+
         function preload() {
             this.game.load.spritesheet('chara_fat', 'assets/gfx/chara_fat.png', 64, 96);
         }
 
         function create() {
+            this.game.state.add('Boot', Boot);
+            this.game.state.add('Preload', Preloader);
+            this.game.state.add('Game', Game);
+
+            this.game.state.start('Game');
         }
 
         function init() {
+
+            // Activate plugins.
+            this.game.add.plugin(Phaser.Plugin.Tiled);
+
         }
 
-        game.state.add('Boot', Boot);
-        game.state.add('Preload', Preloader);
-        game.state.add('Game', Game);
-
-        game.state.start('Game');
-
         function update () {
-            manager.update(1);
         }
     });
 }());
