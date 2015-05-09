75db75b4f603540281b3c92b573957dfec03939c
Maxime AILLOUD <maxime.ailloud@gmail.com>
2015-05-09 15:45:49 +0200

    Reorder methods

--- js/states/upgrade.js
+++ js/states/upgrade.js
@@ -9,13 +9,13 @@ function (
 
     Upgrade.prototype = {
 
-        update: function () {
-        },
-
         create: function () {
             this.game.add.sprite(0, 0, 'upgrade_menu_back_ground');
             this.game.add.sprite(0, 0, 'upgrade_menu_middleground');
             this.game.add.sprite(0, 0, 'upgrade_menu_foreground');
+        },
+
+        update: function () {
         },
 
     };
